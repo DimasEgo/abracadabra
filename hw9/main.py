c36cf0ab3aeb46fe5dfb4d24ee00ea86d867fd34
DimasEgo <105501818+DimasEgo@users.noreply.github.com>
2022-11-30 10:48:33 +0100

    Add files via upload

--- hw9/main.py
+++ hw9/main.py
@@ -1,13 +1,49 @@
-from sqlalchemy.orm import joinedload
-from models import session, Contact, Address, Email, Phone
+import argparse
+import sys
+from sqlalchemy.exc import SQLAlchemyError
+
+from src.seed import add_user, delete_user, get_all_users, get_user, update_user, add_random_users
+
+ACTIONS = {get_all_users: 'show', add_user: 'add', add_random_users: 'add_random',
+           update_user: 'update', delete_user: 'remove', get_user: 'get'}
 
 
-def get_all_names():
-    contacts = session.query(Contact).options(joinedload("cont_address")).all()
-    for contact in contacts:
-        print(vars(contact))
-        print(f'{[f" address is {n.address_ad}" for n in contact.cont_address]}')
+def main():
+    parser = argparse.ArgumentParser(description='Address Book')
+    parser.add_argument('--action', help='Command: add, update, get, remove, show, add_random')
+    parser.add_argument('--id', help='User ID')
+    parser.add_argument('--name', help='User name')
+    parser.add_argument('--phone', help='User phone')
+    parser.add_argument('--email', help='User email')
+    parser.add_argument('--address', help='User address')
+
+    args = parser.parse_args()
+    my_args = vars(args)
+
+    command = my_args.pop('action')
+
+    for k, v in ACTIONS.items():
+        if command == v:
+            try:
+                print(k(**my_args))
+            except SQLAlchemyError as e:
+                print(e)
+            except Exception as e:
+                print(e)
+            finally:
+                sys.exit(0)
+    if command != 'show':
+        try:
+            print(get_all_users())
+        except SQLAlchemyError as e:
+            print(e)
+        except Exception as e:
+            print(e)
+        finally:
+            sys.exit(0)
 
 
 if __name__ == '__main__':
-    get_all_names()
+    main()
+
+
