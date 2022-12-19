115016aa616fb45f4f7cdc9add9d2602c68d03a1
DimasEgo <105501818+DimasEgo@users.noreply.github.com>
2022-12-19 15:52:47 +0100

    Add files via upload

---
+++ AIOHTTP/src/urls.py
@@ -0,0 +1,5 @@
+weather_url = [
+    'https://www.timeanddate.com/weather/ukraine/kyiv',
+    'https://www.weather-forecast.com/locations/Kyiv/forecasts/latest',
+    'https://www.msn.com/en-us/weather/forecast/in-Kyiv,Kyiv-City',
+]
