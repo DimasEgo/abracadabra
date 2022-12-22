7d631e0f6a718df2fdf31433af8efa27329975e6
DimasEgo <105501818+DimasEgo@users.noreply.github.com>
2022-12-22 09:35:01 +0100

    Delete AIOHTTP directory

--- AIOHTTP/src/routes.py
+++
@@ -1,5 +0,0 @@
-from src import views
-
-
-def setup_routes(app):
-    app.router.add_get('/', views.index)
--- AIOHTTP/src/urls.py
+++
@@ -1,5 +0,0 @@
-weather_url = [
-    'https://www.timeanddate.com/weather/ukraine/kyiv',
-    'https://www.weather-forecast.com/locations/Kyiv/forecasts/latest',
-    'https://www.msn.com/en-us/weather/forecast/in-Kyiv,Kyiv-City',
-]
